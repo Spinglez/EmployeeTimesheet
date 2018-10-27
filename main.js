a4a0fd8a563332ef601c4b9fb1ef1f5dd7d914ba
ScottE <scotteratigan@gmail.com>
2018-10-27 10:57:30 -0700

    added actual click event

--- main.js
+++ main.js
@@ -3,10 +3,24 @@ console.log("I'm linked.");
 function appendEmployeeData() {
 	console.log("I submit!");
 	console.log($(this));
-	let empName = $('#name').val().trim();
-	let empStartDate = $('#start-date').val().trim();
-	let empRate = $('#rate').val().trim();
-	let comment = $('#comment').val().trim();
+	employeeData.employeeName = = $('#name').val().trim();
+	employeeData.role = $('#role').val().trim();
+	employeeData.startDate = $('#start-date').val().trim();
+	employeeData.monthlyRate = $('#rate').val().trim();
+
+	if (employeeData.employeeName && employeeData.role && employeeData.startDate && employeeData.monthlyRate)
+	{
+		database.push({
+			'name':  employeeData.employeeName,
+			'role':  employeeData.role,
+			'start': employeeData.startDate,
+			'rate':  employeeData.monthlyRate
+		})
+	}
+	else
+	{
+		console.log("You need to enter all fields!");
+	}
 }
 
 $(document.documentBody).on('click', '#submit', appendEmployeeData)
