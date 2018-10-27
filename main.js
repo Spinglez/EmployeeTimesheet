3efdadf37fee4d624220c1cc54532dce617f544c
Spinglez <42229372+Spinglez@users.noreply.github.com>
2018-10-27 10:44:14 -0700

    added link to firebase, orginized the JS  links to the bottom of the HTML

---
+++ main.js
@@ -0,0 +1,11 @@
+
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyCbZfl98gXYadV58TnT6bZjKtwLoJGzgoM",
+    authDomain: "employee-timeclock-12d26.firebaseapp.com",
+    databaseURL: "https://employee-timeclock-12d26.firebaseio.com",
+    projectId: "employee-timeclock-12d26",
+    storageBucket: "employee-timeclock-12d26.appspot.com",
+    messagingSenderId: "713247075558"
+  };
+  firebase.initializeApp(config);
